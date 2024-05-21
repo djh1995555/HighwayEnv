27c55ba2243fad8b576d43dd84f37a585005728b
dongjianhao <dongjianhao@xiaomi.com>
2024-05-21 19:08:59 +0800

    load controller rl-based

--- vehicle_simulator/vehicle_simulator.py
+++ vehicle_simulator/vehicle_simulator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import shutil
 import yaml
 import argparse
 import datetime
@@ -8,18 +9,26 @@ import gymnasium as gym
 from collections import OrderedDict
 from controller.pid_controller import PIDController
 from utils.report_generator import ReportGenerator
+from stable_baselines3 import SAC
 
 class VehicleSimulator():
     def __init__(self, args, epoch = 0, train_mode = False):
-        simulator_config_filepath = os.path.join(dir_path,'config', args.config)
-        with open(simulator_config_filepath, 'r') as f:
+        self._simulator_config_filepath = os.path.join(dir_path,'config', args.config)
+        with open(self._simulator_config_filepath, 'r') as f:
             self._simulator_config = yaml.load(f,Loader=yaml.Loader)
         env_name = self._simulator_config["env_name"]
         self._output_dir = os.path.join(dir_path,'output', env_name, '{}'.format(datetime.datetime.now()))
         self._model_dir = os.path.join(dir_path,'model', env_name, '{}'.format(datetime.datetime.now()))
-
         self._env = gym.make(env_name, render_mode="rgb_array")
-        self._controller = PIDController()
+
+        if(self._simulator_config['control_type'] == 'classic'):
+            if(self._simulator_config['controller_type'] == 'pid'):
+                self._controller = PIDController()
+        elif(self._simulator_config['control_type'] == 'rl_based'):
+            if(self._simulator_config['model_type'] == 'sac'):
+                model_filepath = os.path.join(dir_path,self._simulator_config["sac_model_filepth"])
+                self._controller = SAC.load(model_filepath)
+
         self._target_signals = []
         for sub_panel, list in self._simulator_config["plot_target_signal"].items():
             for signal_name in list:
@@ -40,11 +49,14 @@ class VehicleSimulator():
         control_output = {}
         while not done and count < 320:
             # positive -> right
-            if("lat_err" in info["debug_info"]):
-                action = self._controller.compute_control_cmd(info["debug_info"], control_output)
-            else:
-                action = [0]
-            # action = [0.1]
+            if(self._simulator_config['control_type'] == 'classic'):
+                if("lat_err" in info["debug_info"]):
+                    action = self._controller.compute_control_cmd(info["debug_info"], control_output)
+                else:
+                    action = [0]
+            elif(self._simulator_config['control_type'] == 'rl_based'):
+                action, _states = self._controller.predict(obs, deterministic=True)
+                control_output['lat_cmd'] = action[0]
             obs, reward, done, truncated, info = self._env.step(action)
             
             self.construct_debug_info(info["debug_info"],control_output)
@@ -53,6 +65,11 @@ class VehicleSimulator():
         output_name = os.path.join(self._output_dir, "report.html")
         self.select_target_signal()
         self._report_generator.plot(self._data_selected, output_name)
+        self.cpoy_config_file()
+
+    def cpoy_config_file(self):
+        tgt_config = os.path.join(self._output_dir,args.config)
+        shutil.copyfile(self._simulator_config_filepath, tgt_config)
 
     def construct_debug_info(self, debug_info, control_output):
         for signal_name in self._target_signals: 
